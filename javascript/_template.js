e44f2550c5b9fee884d224078064f61f33e0af2f
Cleverton Almeida <clevertoncas@gmail.com>
2022-10-02 06:49:18 -0300

    read inputs template

---
+++ javascript/_template.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const currentFileName = path.parse(__filename).name;
+const input = fs.readFileSync(`./inputs/${currentFileName}.txt`, 'utf-8');
+const lines = input.split('\r\n');
+
+// ???? - Hello World!
+// Resolução
+console.log('Hello World!');
